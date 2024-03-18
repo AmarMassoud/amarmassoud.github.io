f3c6d6f78e66e1658642e8b890f395df43b57973
W4rd2 <Wardfakhouri2004@gmail.com>
2024-03-19 00:51:03 +0300

    finished product page

--- pages/buyer/product-page/product-page.js
+++ pages/buyer/product-page/product-page.js
@@ -1,7 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     var widnowWidth = window.innerWidth;
-    const smallScreen=widnowWidth > 600;
+    const smallScreen=widnowWidth < 600;
     const body=document.querySelector("body")
     body.className="px-7"
 
@@ -26,38 +26,48 @@ let products=[]
     };
 
     let desiredQuanitiy=1;
+    const decrementQuanitiy=()=>{
+        if(desiredQuanitiy>1)
+        desiredQuanitiy-=1;
+        renderProductDetails();
+    };
+    const incrementQuanitiy=()=>{
+        if(desiredQuanitiy<currentProduct.stock)
+        desiredQuanitiy+=1;
+        renderProductDetails();
+    };
 
 
 const renderProductDetails = () => {
-    
+    const productInStock=currentProduct.stock>1;
 
     const producDetailsDiv= document.querySelector("#product-details");
     producDetailsDiv.replaceChildren();
-    producDetailsDiv.className = "w-full px-10 py-20 flex " + (smallScreen ? "flex-row justify-around" : "flex-col");
+    producDetailsDiv.className = "w-full px-10 py-20 flex  " + (smallScreen ?"flex-col space-y-16": "flex-row justify-start space-x-32" );
 
 
     const imagesColumn=document.createElement("div")
-    imagesColumn.className="flex flex-col mr-20 bg-custom-gray px-20 py-[2.8rem] rounded-[1.5rem]";
+    imagesColumn.className="flex flex-col bg-custom-gray px-[5rem] py-[2.8rem] rounded-[1.5rem] content-center min-w-[32rem]";
 
     const displayedImg= document.createElement("img");
     displayedImg.src=currentImage;
-    displayedImg.className="object-contain h-96 ";
+    displayedImg.className="object-contain h-fit ";
     
 
     const imageBackground=document.createElement("div");
-    imageBackground.className="border-5 border-red-600 bg-white shadow-md rounded-2xl h-96 w-96 bg-center"
+    imageBackground.className="border-5 border-red-600 bg-white shadow-md rounded-2xl h-fit w-fit bg-center"
     
 
     imageBackground.appendChild(displayedImg);
 
 
     imagesDiv=document.createElement("div");
-    imagesDiv.className="flex flex-row place-content-center w-96 ";
+    imagesDiv.className="flex flex-row justify-center w-[32rem] mt-10";
     images.forEach((image) => {
         if(image != currentImage){
             const miniImage = document.createElement("img");
             miniImage.src = image;
-            miniImage.className = "saturate-[0%] w-[4.5em] h-full object-scale-down transition-all duration-[ease]  cursor-pointer rounded-xl  hover:saturate-[100%] mr-2 hover:w-[25%] m-10";
+            miniImage.className = "saturate-[0%] w-[4rem] h-full object-scale-down transition-all duration-[ease]  cursor-pointer hover:saturate-[100%]  hover:w-[25%] ";
             
             miniImage.addEventListener("click",()=>{
                 currentImage=image;
@@ -73,7 +83,7 @@ const renderProductDetails = () => {
 
 
     const detailsDiv=document.createElement("div");
-    detailsDiv.className="flex flex-col  pt-10 space-y-12 ";
+    detailsDiv.className="flex flex-col  space-y-[4.5rem] w-[37rem]";
 
     const prouctTitle=document.createElement("h2")
     prouctTitle.textContent=currentProduct.title;
@@ -81,21 +91,24 @@ const renderProductDetails = () => {
 
     const productDescription=document.createElement("p");
     productDescription.textContent=currentProduct.description;
-    productDescription.className="font-light text-2xl min-h-20 "
+    productDescription.className="font-light text-2xl min-h-16 "
 
     // categoriesDiv=document.createElement("div");
     // categoriesDiv.className="flex flex-row gap3 "
 
-    const category=document.createElement("p");
-    category.textContent=currentProduct.category;
-    category.className="badge badge-lg badge-outline   text-lg px-5 py-4 "
+    const category=document.createElement("a");
+    category.textContent=` More from ${currentProduct.category.charAt(0).toUpperCase() +currentProduct.category.slice(1)} category`;
+    category.className="badge badge-lg badge-outline text-lg px-5 py-4 rounded-xl border-0 hover:cursor-pointer bg-custom-red text-white font-bold underline self-end"
+    category.addEventListener("click",()=>{
+        goToCategoryPage(currentProduct);
+    })
 
     // categoriesDiv.appendChild(category)
 
     const sellerInfo=document.createElement("div");
     sellerInfo.className="flex flex-row gap3 align-center "
 
-    const profileIcon = document.createElement("div"); // Assuming it's a div
+    const profileIcon = document.createElement("div"); 
     profileIcon.id = "profile-icon";
     profileIcon.className = "flex justify-center items-center rounded-full bg-custom-red ";
     profileIcon.style.width = '1rem';
@@ -116,7 +129,7 @@ const renderProductDetails = () => {
     sellerInfo.appendChild(poducedBy);
 
     const priceQuanitityDiv=document.createElement("div")
-    priceQuanitityDiv.className="flex flex-row justify-between pr-5   "
+    priceQuanitityDiv.className="flex flex-col justify-between pr-5 space-y-5"
 
     const price=document.createElement("p");
     price.textContent="$"+currentProduct.price;
@@ -124,89 +137,59 @@ const renderProductDetails = () => {
 
 
     const quantityDiv=document.createElement("div");
+    quantityDiv.className="flex flex-col space-y-0"
 
 
     const quantity=document.createElement("p");
-    quantity.textContent=currentProduct.textContent=currentProduct.stock +" items left";
-    quantity.className="font-bold text-lg underline"
+    // quantity.textContent=currentProduct.textContent=currentProduct.stock +" items left";
+    quantity.textContent="Availability:";
+    quantity.className=" text-xl pl-1"
 
     quantityDiv.appendChild(quantity);
 
-    const dropdown = document.createElement('div');
-    dropdown.className = 'dropdown dropdown-hover my-1 ';
+    const containerDiv = document.createElement('div');
+    containerDiv.className = 'custom-number-input h-10 w-32 ';
     
-    const button = document.createElement('div');
-    button.setAttribute('tabindex', '0');
-    button.setAttribute('role', 'button');
-    button.className = 'btn m-1';
-    button.textContent = desiredQuanitiy;
-    button.className="btn flex flex-row font-bold justify-between w-[6rem]"
+    
+    const inStock=document.createElement("label");
+    inStock.textContent= (productInStock?"In Stock":"Not In Stock")
+    inStock.className="ml-1  font-bold text-sm " + (productInStock?"text-green-500":"text-red-500")
+    
+    const flexDiv = document.createElement('div');
+    flexDiv.className = 'flex flex-row mt-5  h-10 w-fit rounded-lg relative bg-custom-gray mt-1 border '+ (productInStock?"":"opacity-50");
+    
+    const decrementButton = document.createElement('button');
+    decrementButton.className =  `${(productInStock?`hover:bg-zinc-300`:`hover:cursor-default`)} h-full w-10 rounded-l `;
+    decrementButton.innerHTML = `<span class="m-auto text-2xl  font-thin text-gray-600  ${(productInStock?`hover:text-black `:``)}">-</span>`;
+    
+    const input = document.createElement('input');
+    input.type = 'counter';
+    input.readOnly = true;
+    input.className = ' outline-none text-center w-10 bg-white font-semibold text-md  focus:text-black md:text-base cursor-default flex items-center text-gray-700';
+    input.value = (productInStock?desiredQuanitiy:"X");
+    
+    const incrementButton = document.createElement('button');
+    incrementButton.className =  `${(productInStock?`hover:bg-zinc-300`:`hover:cursor-default`)} h-full w-10 rounded-l `;
+    incrementButton.innerHTML = `<span class="m-auto text-2xl  font-thin text-gray-600  ${(productInStock?`hover:text-black`:``)}">+</span>`;
+    
+    decrementButton.addEventListener("click",()=>{
+    decrementQuanitiy()
+    }
+    );
+    incrementButton.addEventListener("click",()=>{
+      incrementQuanitiy()  
+    })
+
+    flexDiv.appendChild(decrementButton);
+    flexDiv.appendChild(input);
+    flexDiv.appendChild(incrementButton);
+    
+    containerDiv.appendChild(inStock);
+    containerDiv.appendChild(flexDiv);
+    
+    quantityDiv.appendChild(containerDiv);
 
 
-    const svgNS = "http://www.w3.org/2000/svg";
-    const svg = document.createElementNS(svgNS, "svg");
-    
-    // Step 2: Set attributes for the SVG
-    svg.setAttributeNS(null, "width", "24");
-    svg.setAttributeNS(null, "height", "24");
-    svg.setAttributeNS(null, "viewBox", "0 0 24 24");
-    svg.setAttributeNS(null, "fill", "none");
-    svg.setAttributeNS(null, "stroke", "currentColor");
-    svg.setAttributeNS(null, "stroke-width", "2");
-    svg.setAttributeNS(null, "stroke-linecap", "round");
-    svg.setAttributeNS(null, "stroke-linejoin", "round");
-    svg.setAttribute("class", "icon icon-tabler icons-tabler-outline icon-tabler-arrow-down");
-    
-    // Path 1
-    const path1 = document.createElementNS(svgNS, "path");
-    path1.setAttributeNS(null, "stroke", "none");
-    path1.setAttributeNS(null, "d", "M0 0h24v24H0z");
-    path1.setAttributeNS(null, "fill", "none");
-    
-    // Path 2
-    const path2 = document.createElementNS(svgNS, "path");
-    path2.setAttributeNS(null, "d", "M12 5l0 14");
-    
-    // Path 3
-    const path3 = document.createElementNS(svgNS, "path");
-    path3.setAttributeNS(null, "d", "M18 13l-6 6");
-    
-    // Path 4
-    const path4 = document.createElementNS(svgNS, "path");
-    path4.setAttributeNS(null, "d", "M6 13l6 6");
-    
-    // Step 3: Append child elements to the SVG
-    svg.appendChild(path1);
-    svg.appendChild(path2);
-    svg.appendChild(path3);
-    svg.appendChild(path4);
-    
-    button.appendChild(svg)
-    
-    dropdown.appendChild(button);
-    
-    const dropdownContent = document.createElement('ul');
-    dropdownContent.setAttribute('tabindex', '0');
-    dropdownContent.className = 'dropdown-content z-[1] menu bg-custom-gray rounded-box w-[6rem] ';
-    dropdown.appendChild(dropdownContent);
-
-    arrayRange=Array(currentProduct.stock).keys()
-    arrayRange.forEach(itemText => {
-        itemText+=1
-        const li = document.createElement('li');
-        const a = document.createElement('a');
-        a.textContent = itemText;
-        li.appendChild(a);
-        li.addEventListener("click",()=>{
-            desiredQuanitiy = itemText;
-            renderProductDetails();
-            console.log(desiredQuanitiy)
-        });
-        li.className="font-bold w-fit"
-        dropdownContent.appendChild(li);
-      });
-
-      quantityDiv.appendChild(dropdown);
 
 
     priceQuanitityDiv.appendChild(price);
@@ -216,19 +199,22 @@ const renderProductDetails = () => {
 
 
     const addToCartButton=document.createElement("button");
-    addToCartButton.textContent="Add to Cart";
-    addToCartButton.className="rounded-xl bg-custom-red max-w-fit py-2 px-6 text-white text-center font-bold"
-    addToCartButton.addEventListener("click",addToCart(currentProduct))
+    addToCartButton.textContent=(productInStock?'Add to Cart':"Out of Stock");
+    addToCartButton.className="rounded-xl  max-w-fit py-2 px-6  text-center font-bold border   " +(productInStock?'text-white bg-custom-red border-white hover:bg-red-600 hover:animate-spin  transition-all duration-[ease]  ':" border-black bg-custom-gray text-black hover:cursor-default");
+    addToCartButton.addEventListener("click",()=>{if(productInStock)addToCart(currentProduct)})
 
 
     
+    if(!smallScreen)detailsDiv.appendChild(category);
     detailsDiv.appendChild(prouctTitle);
     detailsDiv.appendChild(productDescription);
-    detailsDiv.appendChild(category);
     detailsDiv.appendChild(priceQuanitityDiv);
     detailsDiv.appendChild(sellerInfo);
     detailsDiv.appendChild(addToCartButton);
 
+    if(smallScreen)
+    producDetailsDiv.appendChild(category);
+    
     producDetailsDiv.appendChild(imagesColumn);
     
     //   const borderLine=document.createElement("div");
@@ -239,11 +225,14 @@ const renderProductDetails = () => {
 };
 
 const renderProductCard=(product)=>{
+    const inStock=product.stock>1;
     const card = document.createElement('div');
-    card.className = 'card w-[20rem] bg-base-100 shadow-xl';
+    card.className = 'card w-[20rem] bg-base-100 shadow-xl hover:bg-custom-gray hover:cursor-pointer';
     
     const figure = document.createElement('figure');
+    figure.className="h-[18rem] "
     const img = document.createElement('img');
+    img.className='  h-[18rem] object-contain  self-start'
     img.src = product.images[0]; 
     img.alt = product.title;
     figure.appendChild(img);
@@ -263,21 +252,22 @@ const renderProductCard=(product)=>{
     cardActions.className = 'card-actions justify-end';
     
     const button = document.createElement('button');
-    button.className = 'btn btn-primary';
-    button.textContent = 'Add to Cart';
-    button.addEventListener("click",addToCart(currentProduct))
+    button.className = 'btn btn-primary mb-6 mr-4   ' +(inStock?'hover:bg-custom-red hover:text-white':"hover:cursor-default");
+    button.textContent = (inStock?'Add to Cart':"Out of Stock");
+    button.addEventListener("click",()=>{
+        if(inStock)
+        addToCart(currentProduct)})
     
     cardActions.appendChild(button);
     cardBody.appendChild(title);
     cardBody.appendChild(price);
-    cardBody.appendChild(cardActions);
     card.appendChild(figure);
     card.appendChild(cardBody);
+    card.appendChild(cardActions);
 
-    card.addEventListener("click",()=>{
+    figure.addEventListener("click",()=>{
         updateProduct(product)
-        renderProductDetails();
-    renderMoreProductsByCategory();});
+        rerenderPage();});
 
     return card;
 }
@@ -289,7 +279,7 @@ const renderMoreProductsByCategory=() =>{
     allCategoryProducts=allCategoryProducts.filter(product => product.category === category);
     let categoryProducts=[];
     
-        for(let i=0; i<3 ;i++){
+        for(let i=0; i<(smallScreen? 3 :5) ;i++){
             categoryProducts.push(allCategoryProducts[i]);
         }
   
@@ -306,7 +296,7 @@ const renderMoreProductsByCategory=() =>{
     moreProductsDiv.appendChild(title)
 
     const productsDiv=document.createElement("div");
-    productsDiv.className="flex flex-row justify-around w-full "
+    productsDiv.className="flex flex-row justify-start w-full space-x-10 "
 
     categoryProducts.forEach((product)=>{
         productsDiv.appendChild(renderProductCard(product));
@@ -319,6 +309,15 @@ const renderMoreProductsByCategory=() =>{
     const addToCart=(product)=>{
         //todo
     };
+    const goToCategoryPage=(product)=>{
+        
+    };
+    const rerenderPage=()=>{
+        renderProductDetails();
+    renderMoreProductsByCategory();
+    };
+
+
     renderProductDetails();
     renderMoreProductsByCategory();
 
