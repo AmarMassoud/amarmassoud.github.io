c59c4a549c519f1fedf37f3a193250298ade13f7
W4rd2 <Wardfakhouri2004@gmail.com>
2024-03-29 04:02:29 +0300

    .

--- pages/buyer/purchase-history/purchase-history.js
+++ pages/buyer/purchase-history/purchase-history.js
@@ -1,4 +1,19 @@
+
 document.addEventListener('DOMContentLoaded', async() => {
+document.addEventListener("DOMContentLoaded", async () => {
+    const body=document.querySelector("body")
+    body.className="px-7"
+  const renderPurchaseHistory = async () => {
+    const title = document.querySelector("#order-history-title");
+    title.className="text-5xl underline my-7";
+
+    const dealsDiv = document.querySelector("#card-div");
 
     
+});
+
+};
+
+
+  renderPurchaseHistory();
 });
