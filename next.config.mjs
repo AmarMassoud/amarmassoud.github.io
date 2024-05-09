7ac7a9f602798b2d27856b52f037aef480a669ca
Amar Walid <49458620+AmarMassoud@users.noreply.github.com>
2024-05-09 23:59:56 +0300

    last commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',  // Add this line to enable static export
     async redirects() {
         return [
             {
