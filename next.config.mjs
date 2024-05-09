80ba4190ab499e72e0cf60aebb61ef6c19934cf1
Amar Walid <49458620+AmarMassoud@users.noreply.github.com>
2024-05-09 21:57:40 +0300

    made it so it redirects to the index.html file immediately

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/index.html',
+                permanent: true,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
