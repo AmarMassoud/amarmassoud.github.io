a67009ffc659549138af840fdd18be80e3d4e670
Amar Walid <49458620+AmarMassoud@users.noreply.github.com>
2024-05-10 14:06:57 +0300

    finished seeding for bank and user and address

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -8,26 +8,15 @@ const seed = async () => {
         const data = fs.readFileSync('./public/data/users.json', 'utf8');
         const users = JSON.parse(data);
         for (let user of users) {
-            console.log(user)
-            const addresses = user.addresses;
-            for (let address of addresses) {
-                prisma.address.create({
-                    data: {
-                        name: address.name, userId: user.id, address: address.address, city: address.city, postalCode: address.postalCode, state: address.state
-                    }
-
-                })
-
-            } // trying seeding again
-
             const bank = user.bank;
-            prisma.bank.create({
+            const bankSchema = await prisma.bank.create({
                 data: {
-                    ...bank, userId: user.id
+                    ...bank
                 }
             })
 
-            prisma.user.create({
+
+            await prisma.user.create({
                 data: {
                     firstName: user.firstName,
                     lastName: user.lastName,
@@ -36,11 +25,29 @@ const seed = async () => {
                     image: user.image,
                     role: user.role,
                     balance: user.balance,
-
-
+                    bankId: bankSchema.id,
+                    id: user.id.toString()
                 }
             })
-            }
+
+            console.log(user)
+            const addresses = user.addresses;
+            for (let address of addresses) {
+                await prisma.address.create({
+                    data: {
+                        name: address.name,
+                        userId: user.id.toString(),
+                        address: address.address.address,
+                        city: address.address.city,
+                        postalCode: address.address.postalCode,
+                        state: address.address.state
+                    }
+
+                })
+
+            } // trying seeding again
+
+        }
 
 
 
